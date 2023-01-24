93938e62147a9e382f8913642fa304a8242aa33c
vstrom650xt <cerveramonzo@gmail.com>
2023-01-24 18:00:38 +0100

    updated tablero

--- src/main/java/Prueba.java
+++ src/main/java/Prueba.java
@@ -7,11 +7,9 @@ public class Prueba {
     public static void main(String[] args) {
         Coordinate c1 = new Coordinate('g',6);
 
-        Piece cb= new Piece(Piece.Type.BLACK_KNIGHT,new Cell(new Board(),c1));
-        Piece cb2= new Piece(Piece.Type.WHITE_KNIGHT,new Cell(new Board(),c1));
 
-        System.out.println(cb);
-        System.out.println(cb2);
+        Board b = new Board();
+        System.out.println(b);
 
 
     }
